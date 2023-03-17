59846f92321985667e1221b30d0a725962be0355
Dustin Martin <dustin@dmartin.io>
2023-03-17 13:56:51 -0400

    Clean up CLI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::time::Duration;
+use std::{collections::HashMap, time::Duration};
 
+use anyhow::Context;
 use clap::Parser;
 use docker_reaper::{reap_containers, ReapContainersConfig};
 use tokio::time::sleep;
@@ -8,12 +9,12 @@ use tokio::time::sleep;
 #[command(after_help = "Note: <duration> values accept Go-style duration strings (e.g. 1m30s)")]
 struct Args {
     /// Interval to wait after reaping containers.
-    #[arg(long, value_name = "duration", default_value_t = String::from("60s"))]
-    every: String,
+    #[arg(long, value_name = "duration", value_parser = parse_duration, default_value = "60s")]
+    every: Duration,
     /// Only reap containers once. Conflicts with "--every".
     #[arg(long, conflicts_with = "every")]
     once: bool,
-    /// Print output without actually removing containers or networks.
+    /// Log output without actually removing containers or networks.
     #[arg(long, short = 'd')]
     dry_run: bool,
     /// Only containers older than this duration will be eligible for reaping.
@@ -29,9 +30,10 @@ struct Args {
         short = 'f',
         // todo: https://github.com/clap-rs/clap/issues/2389
         help = "Additional Docker-engine supported container filters (https://docs.docker.com/engine/reference/commandline/ps/#filter). Can be specified multiple times",
-        value_name = "name=value"
+        value_name = "name=value",
+        value_parser = parse_filter
     )]
-    filters: Vec<String>,
+    filters: Vec<Filter>,
     /// Also attempt to remove the networks associated with reaped containers.
     #[arg(long)]
     reap_networks: bool,
@@ -40,23 +42,61 @@ struct Args {
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     let args = Args::parse();
-    let config = ReapContainersConfig {};
+    let config = ReapContainersConfig {
+        dry_run: args.dry_run,
+        min_age: args.min_age,
+        max_age: args.max_age,
+        filters: {
+            args.filters.iter().fold(HashMap::new(), |mut acc, f| {
+                acc.entry(f.name.clone()).or_default().push(f.value.clone());
+                acc
+            })
+        },
+        reap_networks: args.reap_networks,
+    };
     if args.once {
         reap_containers(&config).await;
     } else {
-        let sleep_ns = match go_parse_duration::parse_duration(&args.every) {
-            Ok(ns) => ns,
-            Err(_) => anyhow::bail!("failed to parse \"since\" value"),
-        };
-        if sleep_ns < 1 {
-            anyhow::bail!("\"since\" must be a positive duration")
-        }
-        let sleep_ns: u64 = sleep_ns.try_into()?;
-        let sleep_duration = Duration::from_nanos(sleep_ns);
         loop {
             reap_containers(&config).await;
-            sleep(sleep_duration).await;
+            sleep(args.every).await;
         }
     }
     Ok(())
+}
+
+#[derive(Clone, Debug)]
+pub(crate) struct Filter {
+    name: String,
+    value: String,
+}
+
+impl Filter {
+    pub(crate) fn new(name: &str, value: &str) -> Self {
+        Self {
+            name: String::from(name),
+            value: String::from(value),
+        }
+    }
+}
+
+fn parse_filter(value: &str) -> Result<Filter, anyhow::Error> {
+    let err_msg = "filters must be in NAME=VALUE(=VALUE) format";
+    let (name, value) = value.split_once('=').context(err_msg)?;
+    if name.is_empty() || value.is_empty() {
+        return Err(anyhow::anyhow!(err_msg));
+    }
+    Ok(Filter::new(name, value))
+}
+
+fn parse_duration(value: &str) -> Result<Duration, anyhow::Error> {
+    let sleep_ns = match go_parse_duration::parse_duration(value) {
+        Ok(ns) if ns < 1 => {
+            anyhow::bail!("must be a positive duration: {}", value);
+        }
+        Ok(ns) => ns,
+        Err(_) => anyhow::bail!("failed to parse duration: {}", value),
+    };
+    let sleep_ns: u64 = sleep_ns.try_into()?;
+    Ok(Duration::from_nanos(sleep_ns))
 }
